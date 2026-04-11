36cd4179062bbe1a9c6df209711639e54d412b22
Dave Williams | DitroniX | G8PUO <20687856+DitroniX@users.noreply.github.com>
2026-04-11 15:29:48 +0100

    Code Examples

---
+++ Code/Arduino/GPIO_P5_Test/GPIO_P5_Test.ino
@@ -0,0 +1,114 @@
+/*
+  Dave Williams, DitroniX 2019-2026 (ditronix.net)
+  ESPKnack - IoT ESP32-C6 Based Home and Industrial Controller SDK
+  Features include ESP32-C6, LoRa, Isolated I/Os, RS485, 5-60V, 4-20mA, Relays, DIN SDK
+
+  ESPKnack - Basic Bring Up Test Code - April 2026
+ 
+  Remember!
+  - Set the BOARD to Use ESP32C6 Dev Module (or similar).
+  - You can also set the BAUD rate up to 921600 to speed up flashing.
+  - The SDK does NOT need external power to flash.  It will take Power from the USB 5V.
+  - The Serial Monitor is configured for BAUD 115200
+  
+  The purpose of this test code is to cycle through the various main functions of the board as part of bring up testing.
+  .
+  This test code is OPEN SOURCE and formatted for easier viewing.  Although is is not intended for real world use, it may be freely used, or modified as needed.
+  It is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+  Further information, details and examples can be found on our website or github.com/DitroniX
+
+  * github.com/DitroniX
+  * github.com/DitroniX/ESPKnack-LoRa-Isolated-I-Os-RS485-5-60V-4-20mA-Relays-DIN
+  * github.com/DitroniX/ESPKnack-LoRa-Isolated-I-Os-RS485-5-60V-4-20mA-Relays-DIN/wiki
+  * hackster.io/DitroniX/espknack-lora-isolated-i-os-rs485-5-60v-4-20ma-relays-sdk-b70677
+*/
+
+// Libraries
+#include <Arduino.h>
+#include <Wire.h>
+
+// **************** USER VARIABLES / DEFINES / STATIC / STRUCTURES / CONSTANTS ****************
+
+// Define I2C (Expansion Port)
+#define I2C_SDA 6
+#define I2C_SCL 7
+#define I2C_CLOCK 4000000UL
+
+// TCA6408A
+#define TCA6408A_ADDR 0x21
+#define REG_INPUT 0x00
+#define REG_OUTPUT 0x01
+#define REG_POLARITY 0x02
+#define REG_CONFIG 0x03
+
+// **************** OUTPUTS ****************
+#define LED_Red 22  // Red LED
+
+// **************** FUNCTIONS AND ROUTINES ****************
+
+void writeRegister(uint8_t reg, uint8_t value) {
+  Wire.beginTransmission(TCA6408A_ADDR);
+  Wire.write(reg);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+uint8_t readRegister(uint8_t reg) {
+  Wire.beginTransmission(TCA6408A_ADDR);
+  Wire.write(reg);
+  Wire.endTransmission(false);
+  Wire.requestFrom((uint8_t)TCA6408A_ADDR, (uint8_t)1);
+  return Wire.available() ? Wire.read() : 0xFF;
+}
+
+// **************** SETUP ****************
+void setup() {
+  // Stabalise
+  delay(250);
+
+  // Initialise UART
+  Serial.begin(115200, SERIAL_8N1);  // 115200
+  while (!Serial)
+    ;
+  Serial.println("");
+
+  // Initialise I2C
+  Wire.begin(I2C_SDA, I2C_SCL);
+  Wire.setClock(I2C_CLOCK);  // 400kHz | Standard-mode (Sm) and Fast-mode (Fm) which can go up to 100kHz and 400kHz respectively
+
+  // Initialize outputs to off (all LEDs off)
+  writeRegister(REG_OUTPUT, 0x0F);
+
+  //  Configures the specified LED GPIO as outputs
+  pinMode(LED_Red, OUTPUT);
+
+  // LEDs Default Off State
+  digitalWrite(LED_Red, LOW);
+
+  Serial.println("ESPKnack Bring Up and Test Example Code");
+}
+
+// **************** LOOP ****************
+void loop() {
+
+  // TCA6408 Outputs
+  // P4 = OPTO
+  // P5 = P5
+  // P6 = Relay 2
+  // P7 = Relay 1
+
+  //Ports            76543210
+  uint8_t Output = 0b00100000;
+
+  writeRegister(REG_OUTPUT, Output);
+  delay(250);
+  writeRegister(REG_OUTPUT, 0x00);  // Clear
+
+  // Red
+  digitalWrite(LED_Red, HIGH);
+  delay(100);
+  digitalWrite(LED_Red, LOW);
+  delay(250);
+}
+//
